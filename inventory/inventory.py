be83f0d72b991c2844aeba2db4c709bc98d76560
Chek Yin Choi <chekchek2007@gmail.com>
2021-12-04 00:03:51 +0100

    Use spline in VF and fix bugs in PMF NJ

--- inventory/inventory.py
+++ inventory/inventory.py
@@ -6,15 +6,24 @@ Created on Mon Nov 29 20:21:07 2021
 @author: chek_choi
 """
 from inventory_util import *
+from time import time
+
 
 p  = param()
 r = result()
-
+big_loop_time = time()
 p_err = 100
 while (p_err > p.crit_clear):
+    vf_start = time()
     vfi(r, p)
+    print("done with VFI, time is {}".format(time()-vf_start))
+    print("============================")
+    print(r.s_star)
     inventory_sequence(r, p)
-    r.stat_dist = final_good_dist(r, p)
-    market_clear(r, p)
-    p_err = p_star - p_star_new 
-    p_err = 0
+    r.stat_dist, r.h = final_good_dist(r, p)
+    r.p_star , p_err = market_clear(r, p)
+    w, q = get_price(p, r.p_star)
+    #   p_err = 0
+    
+print("done with all, time is {}".format(time()-big_loop_time))
+
--- inventory/inventory_util.py
+++ inventory/inventory_util.py
@@ -20,7 +20,7 @@ def golden_search(func, bmin, bmax):
     d = bmin + (1. - r)*(bmax -bmin)
     x = d
     fd = func(x)
-    fd = -1.*fc  # is it a typo?
+    fd = -1.*fd  # is it a typo?
     
     for i in range(max_it):
         if fc >= fd:
@@ -53,8 +53,8 @@ class param(object):
         self.del_cost_max = .2198
         self.del_cost_min = 0
         self.cost_dist = stats.uniform(self.del_cost_min , self.del_cost_max)
-        self.z_bar = 1.032  # no uncertainty
-        self.store_cost = .0287
+        self.z_bar = 1.0032  # no uncertainty
+        self.store_cost = .012
         self.NS = 25
         s_min = 0
         s_max = 2.5
@@ -66,187 +66,253 @@ class param(object):
         self.s_grid = np.zeros(self.NS)
         self.s_grid[1:] = 10**np.linspace(lin_min, lin_max, step)
         self.NJ = 10  # number of epriods w/o adjustment
-        self.crit_vf = 1e-6
+        self.crit_vf = 1e-6 # should be -6
         self.crit_clear = 1e-8
         self.crit_used_up = 1e-8
         self.p_max = 3.3
         self.p_min = 3.2
-        
-        
-        @njit
-        def _get_price(self, price):
-            w = self.eta/price
-            ugly = (1-self.beta*(1-self.delta))/self.beta/self.alpha
-            inv_alpha = 1- self.alpha
-            q = (price**(-inv_alpha))*(ugly**self.alpha)*((self.eta/inv_alpha)**inv_alpha)
-            return w, q  # wage and intermediate good price
-        
+
 
 class result(object):
     
     def __init__(self):
         self.m = 1
         self.n = 1
-        self.P = param()
-        self.p_star = (self.P.p_max+self.P.p_min)/2
-        self.w, self.q = self.P._get_price()
-        self.mu = None
-        self.vals_s1 = np.zeros(self.P.NS)
-        self.val_s_star = 0
+        P = param()
+        self.p_star = (P.p_max+P.p_min)/2
+        # self.p_star = 3.25
+        self.p_star_new = 0
+        self.wage, self.q = get_price(P, self.p_star)
+        self.vals_s1 = np.zeros(P.NS)
+        self.vals_s1_new = np.zeros(P.NS)
+        self.val_s_star = 10
+        self.val_s_star_new = 0
+        self.vfunc0 = np.zeros(P.NS)  # the expected values
+        self.vfunc0_new = np.zeros(P.NS)
         self.val_adj = 0  # scalar
-        self.m_arr = np.zeros(self.P.NS)
+        self.s_star = 0
+        self.m_arr = np.zeros(P.NS)
+        self.n_arr = np.zeros(P.NS)
+        self.m_arr_j = np.zeros(P.NJ)
+        self.n_arr_j = np.zeros(P.NJ)
+        self.s_arr = np.zeros(P.NJ)
+        self.adj_pmf = np.zeros(P.NJ)
+        self.stat_dist = np.zeros(P.NJ)
+        self.h = np.zeros(P.NJ)
+        self.threshold_arr = np.zeros(P.NS)
 
+def get_price(param, price):
+    w = param.eta/price
+    ugly = (1-param.beta*(1-param.delta))/param.beta/param.alpha
+    inv_alpha = 1- param.alpha
+    q = (price**(-inv_alpha))*(ugly**param.alpha)*((param.eta/inv_alpha)**inv_alpha)
+    return w, q  # wage and intermediate good price
 
 def vfi(r, p):
-    r.vfunc0 = _init_guess_expected_v0(s1, m)
+    r.vfunc0 = _init_guess_expected_v0(p.s_grid, r.p_star, p.theta_m, p.theta_n, p.eta)
     #TODO: set init v star new
-    while max(np.max(np.abs(r.v_s_star_new- r.v_s_star)), np.max(np.abs(v0_new- v0))) > p.vf_crit:
-        r.v_s_star = r.v_s_star_new
+    vf_err = 100
+    it = 0
+    while vf_err > 1e-5:
         _optimal_m(r, p) # find the value for all s1
-        _optimal_s(r, p) # set v_s_star_new and adjustment value/ v0 in paper
+        # set v_s_star_new and adjustment value/ v0 in paper
+        r.vals_s1_new, r.val_s_star_new, r.s_star, r.val_adj = _optimal_s(p.NS, p.s_grid, r.m_arr, r.n_arr,
+                p.theta_m, p.theta_n, p.store_cost, r.wage, p.beta, r.vfunc0, r.p_star, r.q) 
+        r.threshold_arr = _del_cost_threhold(r, p)
         for si  in range(p.NS):
-            _find_expected_v0(si)  # set new v0 (should be of length NS)
-    #TODO: check corner
+            r.vfunc0_new[si] = _find_expected_v0(si, r, p)  # set new v0 (should be of length NS)
+        #TODO: check corner
+        vf_err = max(np.abs(r.val_s_star_new- r.val_s_star), np.max(np.abs(r.vfunc0_new- r.vfunc0)))
+        r.val_s_star = r.val_s_star_new
+        r.vfunc0 = r.vfunc0_new
+        r.vals_s1 = r.vals_s1_new
+        it += 1
+        # vf_err = 0
+        print("done with vfi {} ,{}".format(vf_err, it))
+        if it == 5000:
+            raise ValueError
             
 
 def inventory_sequence(r, p):
     not_used_up = 1
     # highly doubtful if this is faster than np.interp with njit
     pol_spline = interpolate.UnivariateSpline(p.s_grid, r.m_arr)
+    threshold_spline = interpolate.UnivariateSpline(p.s_grid, r.threshold_arr)
     while (not_used_up > 0):
         #TODO: populate this s1 arr
-        this_s1_arr = np.zeros(p.j_max)
         this_s1 = r.s_star
-        m_arr = np.zeros(p.j_max)
-        s_arr = np.zeros(p.j_max)
-        # adj_cdf = np.ones(p.j_max)  # if not changed (loop breaked )
-        adj_pmf = np.zeros(p.j_max)
-        for ti, time in range(p.j_max):
-            if (this_s1 < p.epsilon):
-                m_arr[ti] = this_s1
-                threshold = _del_cost_threhold(this_s1)
-                adj_pmf[ti]  = p.cost_dist.cdf(threshold)      
-                if ti != p.j_max:
-                    adj_pmf[ti+1]  = 1-np.sum(adj_pmf) 
+        r.m_arr_j = np.zeros(p.NJ)
+        r.s_arr = np.zeros(p.NJ)
+        r.adj_pmf = np.zeros(p.NJ)
+        for ti in range(p.NJ):
+            if (this_s1 < p.crit_used_up):
+                if this_s1 > 0:
+                    r.s_arr[ti] = this_s1
+                    r.m_arr_j[ti] = this_s1
+                    threshold = threshold_spline(0)  # must adjust
+                    r.adj_pmf[ti] = np.sum(r.adj_pmf[:ti]) * p.cost_dist.cdf(threshold)
+                    if ti != p.NJ:
+                        r.adj_pmf[ti + 1] = 1 - np.sum(r.adj_pmf)
+                    else:
+                        r.adj_pmf[ti] += 1 - np.sum(r.adj_pmf)
                 else:
-                    adj_pmf[ti]  += 1-np.sum(adj_pmf) 
-                this_s1_arr[ti] = this_s1
-                this_s1 = 0
+                    r.s_arr[ti] = 0
+                    r.m_arr_j[ti-1] -= this_s1
+                    r.adj_pmf[ti-1] += 1 - np.sum(r.adj_pmf)
                 break
                 # pmf remains zero for later period when all firms adjusted (as all inventory are done)
             else:
+                r.s_arr[ti] = this_s1
                 # no this_s_star have to be solved I think, maybe interp valuefunction
                 # m_arr[ti] =  optimal_m(r, p, this_s1) # find the value for all s1
-                m_arr[ti] = pol_spline(this_s1)
+                r.m_arr_j[ti] = pol_spline(this_s1)
                 # find the share of firms adjusting at time t
-                threshold = _del_cost_threhold(this_s1)
-                adj_pmf[ti]  = p.cost_dist.cdf(threshold)
-                this_s1_arr[ti] = this_s1
-                this_s1 -= m_arr[ti]
-                
+                threshold = threshold_spline(this_s1 - r.m_arr_j[ti])
+                if ti == 0:
+                    r.adj_pmf[ti] = p.cost_dist.cdf(threshold)
+                else:
+                    r.adj_pmf[ti] = np.sum(r.adj_pmf[:ti])*p.cost_dist.cdf(threshold)
+                this_s1 -= r.m_arr_j[ti]
+        r.n_arr_j = _find_n(r.m_arr_j, p.theta_m, p.theta_n, p.eta, r.p_star)
         # if there is next loop (not all used up), increase jmax by 1
-        not_used_up = 1- np.sum(adj_pmf)
+        not_used_up = 1- np.sum(r.adj_pmf)
     
         
     
 def final_good_dist(r, p):
     # use the method where firm differs only in adjustment periods
-    stat_dist = np.zeros(p.NJ)
+    stat_dist = np.ones(p.NJ)/p.NJ  # uniform distributed
     stat_dist[0] = 1.
     adj_cdf = np.cumsum(r.adj_pmf)
     not_adj = 1 - adj_cdf
-    # the grids correspond to the this_s1_arr in inventory_sequence
-    for ti in range(p.NJ):
-        pass
     #TODO: use kierans method
+    # the grids correspond to the this_s1_arr in inventory_sequence
+    # for ti in range(p.NJ):
+    #     pass
     # use transition matrix
-    h = adj_pmf/not_adj  # chance of adjusting at period t
-    trans_mat = np.zeros(p.NJ, p.NJ)
+    h = np.zeros(p.NJ)
+    h[not_adj>0] = r.adj_pmf[not_adj>0]/not_adj[not_adj>0]  # chance of adjusting at period t
+    trans_mat = np.zeros((p.NJ, p.NJ))
+    for ji in range(p.NJ):
+        trans_mat[ji, 0] = h[ji]
+        trans_mat[ji, ji] = 1-h[ji]
     
-    return stat_dist
+    almost_stat = np.linalg.matrix_power(trans_mat, 500)
+    stat_dist = almost_stat.dot(stat_dist)
+    return stat_dist, h
     
         
-def market_clear(p, r):
+def market_clear(r, p):
     # find demand for m, n
-    agg_cap = 0
-    m_demand = 0.
+    interm_demand = 0.
     for ji in range(p.NJ):
         # demand is how much is restocked
-        m_demand += r.h[ji]*r.stat_dist[ji]*(r.s_star - r.s_arr[ji])
-    n_demand = _find_n(m_demand)
+        interm_demand += r.h[ji]*r.stat_dist[ji]*(r.s_star - r.s_arr[ji])
+    # this is wrong, because restock have nothing to do with production
+    n_demand = np.sum(r.n_arr_j*r.stat_dist)
     # find agg capital and labor
-    labor_supply = 1/p.eta
-    final_good_labor = labor_supply - n_demand# given by HH problem
-    agg_cap = _find_agg_cap(final_good_labor, m_demand, p.zbar, p.alpha)
-    # find consumption
+    agg_cap, interm_good_labor = _find_agg_cap_lab(r.q, p.z_bar, p.alpha, r.wage, interm_demand)
+    agg_lab = interm_good_labor+ n_demand
+    # find consumption by the aggregate C=Y-dK
     consum = 0
     for ji in range(p.NJ):
-        pass
+        consum += _final_production(r.m_arr_j[ji], r.n_arr_j[ji], p.theta_m, p.theta_n)  #
+        consum -= p.store_cost*(r.s_arr[ji]-r.m_arr_j[ji])
+        print(consum, "consum")
+        consum *= r.stat_dist[ji]
+
     # update p
+    consum -= agg_cap*p.delta
     new_p = 1/ consum
-    
-def _find_agg_cap(lab, demand, z, alpha):
-    lab_con = lab**(1-alpha)
-    return (demand/z/lab_con)**(1/alpha)
+    if new_p < r.p_star:
+        p.p_max = r.p_star
+    else:
+        p.p_min = r.p_star
+    p_star_new = (p.p_max +p.p_min)/2
+    err = np.abs(r.p_star - p_star_new)
+    return p_star_new, err
+
+@njit
+def _find_agg_cap_lab(q, z, alpha, w, demand):
+    # agg_labor is ugly term 
+    ugly_term = (w/q/z/(1-alpha))**(1/alpha)
+    agg_cap = demand/(ugly_term**(1-alpha)) 
+    interm_lab = ugly_term*agg_cap
+    return agg_cap, interm_lab
         
-# TODO: change to return
-def _optimal_s(r, p):
-    for si, s1 in enumerate(p.s_grid):
-        r.vals_s1[si] =
-    r.val_s_star_new = np.max(r.vals_s1)
-    r.s_star = p.s_grid[np.argmax(r.vals_s1)]
-    r.v_a = r.val_s_star_new
+def _optimal_s(NS, s_grid, m_arr, n_arr, theta_m, theta_n, store_cost, wage, beta, vfunc0, p_star, q):
+    vals_s1 = np.zeros(NS)
+    v0_spline = interpolate.UnivariateSpline(s_grid, vfunc0)
+    for si, s1 in enumerate(s_grid):
+        vals_s1[si] =p_star*_final_net_prod(m_arr[si], n_arr[si], theta_m, 
+                        theta_n, s1, store_cost, wage) + beta*v0_spline(s_grid[si]-m_arr[si])
+    vals_s1_with_cost = -vals_s1+p_star*q*s_grid  # as minimize, *-1
+    v1_spline = interpolate.UnivariateSpline(s_grid, vals_s1_with_cost)
+    # TODO: use spline
+    s_star = optimize.minimize(v1_spline, 1.7, bounds=[(s_grid[0], s_grid[-1])])
+    s_star = s_star.x
+    s_star = s_grid[np.argmin(vals_s1_with_cost)]
+    val_s_star_new = -1.*v1_spline(s_star)
+    val_adj = val_s_star_new
+    return vals_s1, val_s_star_new, s_star, val_adj
     
 
 def _optimal_m(r, p):  # void function
-    
-    def find_m(m, s1):
-        this_val = r.p_star*_final_net_prod(m, n, theta_m, theta_n, s1, store_cost, wage)
-        next_u = p.beta*_find_expected_v0(s1, m)
-        return -(this_val+next_u)  # because the function is minimized
-    for si, s1 in enumerate(s_grid):
-        m0 = s1
-        res = optimize.minimize(find_m, m0, args=(s1), method='Nelder-Mead', tol=p.crit_vf)
+    v0_spline = interpolate.UnivariateSpline(p.s_grid, r.vfunc0)
+    def find_m(m, si, p, r):
+        n = _find_n(m, p.theta_m, p.theta_n, p.eta, r.p_star)
+        this_val = r.p_star*_final_net_prod(m, n, p.theta_m, p.theta_n, s1, p.store_cost, r.wage)
+        next_u = p.beta*v0_spline(p.s_grid[si]-m)
+        return -1.*(this_val+next_u)  # because the function is minimized
+    for si, s1 in enumerate(p.s_grid):
+        m0 = s1/2
+        res = optimize.minimize(find_m, m0, args=(si, p, r), method='Nelder-Mead', tol=p.crit_vf, bounds=[(0, s1)])
         r.m_arr[si] = res.x
-        # find a way to store q (net_prod)
+        if r.m_arr[si] > s1:
+            r.m_arr[si] = s1
+        r.n_arr[si] = _find_n(r.m_arr[si], p.theta_n, p.theta_m, p.eta, r.p_star)
+        # TODO: find a way to store q (net_prod) and s
 
-def _init_guess_expected_v0(s1, m, p_star, theta_m, theta_n, eta):
+@njit
+def _init_guess_expected_v0(s_grid, p_star, theta_m, theta_n, eta):
+    # returns arr of length NS 
     inv_the_n = 1-theta_n
     first = p_star**(1/(1-theta_n))
     second  = (theta_n/eta)**(theta_n/inv_the_n)
-    third = (s1-m)**(theta_m/inv_the_n)
+    third = np.power(s_grid, theta_m/inv_the_n)
     return first*inv_the_n*second*third
-    
+
+@njit
 def _find_n(m, theta_n, theta_m, eta, price):
     pow_i = 1/(1-theta_n)
     return (theta_n*price*(m**theta_m)/eta)**pow_i
 
-
+@njit
 def _final_production(m, n, theta_m, theta_n):
     return (m**theta_m)*(n**theta_n)
 
+@njit
 def _final_net_prod(m, n, theta_m, theta_n, s1, store_cost, wage):  # q
     cost = store_cost*(s1-m) + wage*n
     return _final_production(m, n, theta_m, theta_n) - cost
 
-def _final_prod(z, k, l, alpha):
+@njit
+def _interm_prod(z, k, l, alpha):
     return z*(k**alpha)*(l**(1-alpha))
 
 def _find_expected_v0(stock_idx, r, p):
-    r.threshold = _del_cost_threhold(p.s_grid[stock_idx])
-    r.percent_adj  =p.cost_dist.cdf(r.threshold)
-    first =r.percent_adj*(r.val_adj+r.p_star*p.s_grid[stock_idx]*r.q)
-    area = r.percent_adj*r.threshold
+    threshold = r.threshold_arr[stock_idx]
+    percent_adj  =p.cost_dist.cdf(threshold)
+    first =percent_adj*(r.val_adj+r.p_star*p.s_grid[stock_idx]*r.q)
+    area = percent_adj*threshold/2  # prob of adj * conditional expected cost
     second = r.p_star*r.wage* area
-    third = (1 - r.percent_adj)*r.val_s_star[stock_idx]
+    third = (1 - percent_adj)*r.vals_s1[stock_idx]
     return first - second + third
-    
 
 def _del_cost_threhold(r, p):
-    def find_threshold(threshold, r):
-        lhs = r.v_a - r.p_star*r.wage*threshold
-        rhs = r.val_s_star - r.p_star*r.q*r.s_star
-        return rhs- lhs
-    res = optimize.brentq(find_threshold, a=p.del_cost_min, b=p.del_cost_max, args=(r, p))
-    #TODO:
-    return res.x
+    # return arr of length NS
+    thres_arr = -1. * (r.vals_s1 - r.p_star * r.q * p.s_grid - r.val_adj) / r.p_star / r.wage
+    thres_arr[thres_arr < 0] = 0
+    thres_arr[thres_arr > p.del_cost_max] = p.del_cost_max
+    return thres_arr
+
