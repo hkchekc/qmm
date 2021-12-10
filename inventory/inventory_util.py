dafbe394f7ab220cef394026d2ab63afb95cc2cb
Chek Yin Choi <chekchek2007@gmail.com>
2021-12-10 21:17:51 +0100

    Improve performance by using JIT in finding s*

--- inventory/inventory_util.py
+++ inventory/inventory_util.py
@@ -3,7 +3,7 @@
 import numpy as np
 import math
 from scipy import interpolate, stats, optimize
-from numba import njit
+from numba import njit, jit
 
 class param(object):
     
@@ -14,7 +14,7 @@ class param(object):
         self.theta_m = .4991
         self.theta_n = .3275
         self.delta = .0173
-        self.del_cost_max = .333
+        self.del_cost_max = .2198
         self.del_cost_min = 0
         self.cost_dist = stats.uniform(loc =self.del_cost_min , scale=self.del_cost_max)
         self.z_bar = 1.0032  # no uncertainty
@@ -29,6 +29,7 @@ class param(object):
         step  = self.NS-1
         self.s_grid = np.zeros(self.NS)
         self.s_grid[1:] = 10**np.linspace(lin_min, lin_max, step)
+        # self.s_grid = np.linspace(s_min, s_max, self.NS)
         self.NJ = 12  # number of epriods w/o adjustment
         self.crit_vf = 1e-6 # should be -6
         self.crit_clear = 1e-8
@@ -78,8 +79,6 @@ def get_price(param, price):
     return w, q  # wage and intermediate good price
 
 def vfi(r, p):
-    r.vfunc0 = _init_guess_expected_v0(p.s_grid, r.p_star, p.theta_m, p.theta_n, p.eta)
-    r.vfunc_s1 = r.vfunc0 + r.p_star*0.3
     vf_err = 100
     it = 0
     while vf_err > p.crit_vf:
@@ -172,10 +171,14 @@ def market_clear(r, p):
     # update p
     consum -= agg_cap*p.delta
     new_p = 1 / consum
-    if new_p < r.p_star:
-        p.p_max = float(r.p_star)
+    if np.abs(new_p - r.p_star) < 0.0001:
+        ratio = 0.9
     else:
-        p.p_min = float(r.p_star)
+        ratio = 0
+    if new_p < r.p_star:
+        p.p_max = (1-ratio)*float(r.p_star)+ratio*p.p_max
+    else:
+        p.p_min = (1-ratio)*float(r.p_star)+ratio*p.p_min
     p_star_new = (p.p_max +p.p_min)/2
     err = np.abs(r.p_star - new_p)
     return p_star_new, err
@@ -195,48 +198,59 @@ def _find_agg_cap_lab(q, z, alpha, w, demand):
     interm_lab = demand/(ugly_term**(alpha))
     agg_cap = ugly_term*interm_lab
     return agg_cap, interm_lab
-        
+
+@jit(nopython=False, forceobj=True)
 def _optimal_s(s_grid, p_star, q, vals_s1):
     vals_s1_with_cost = p_star*q*s_grid - vals_s1
     v1_wc_spline = interpolate.CubicSpline(s_grid, vals_s1_with_cost, bc_type="not-a-knot")
     s_star = optimize.minimize_scalar(v1_wc_spline, bracket=(s_grid[0], s_grid[-1]),
-                                      method="bounded", bounds=(s_grid[0], s_grid[-1]), tol=1e-10)
+                                      method="bounded", bounds=(s_grid[0], s_grid[-1]))
     # s_star = optimize.golden(v1_wc_spline, brack=(s_grid[0], s_grid[-1]), tol=1e-10, full_output=True)
     s_star = s_star.x
-    # if s_star > s_grid[-1]:
-    #     s_star = s_grid[-1]
-    # s_star = golden_search(v1_wc_spline, s_grid[0], s_grid[-1])
-    # print(s_star)
     val_adj = -1.*v1_wc_spline(s_star)
     val_s_star_new = val_adj +p_star*q*s_star
     return val_s_star_new, s_star, val_adj
-    
 
 def _optimal_m(r, p):  # void function
-    v0_spline = interpolate.CubicSpline(p.s_grid, r.vfunc0, bc_type="not-a-knot")
-    def find_m(m, si, p, r):
+    v0_spline_out = interpolate.CubicSpline(p.s_grid, r.vfunc0, bc_type="not-a-knot")
+    def _find_m_minimizer(m, si, p, r, v0_spline):
         n = _find_n(m, p.theta_m, p.theta_n, p.eta, r.p_star)
         this_val = r.p_star*_final_net_prod(m, n, p.theta_m, p.theta_n, p.s_grid[si], p.store_cost, r.wage)
         next_u = p.beta*v0_spline(p.s_grid[si]-m)
         return -1.*(this_val+next_u)  # because the function is minimized
     for si, s1 in enumerate(p.s_grid):
-        res = optimize.minimize_scalar(find_m, args=(si, p, r), bracket=(0, s1), method="golden",
-                                          bounds=(0, s1), tol=1e-10)
+        res = optimize.minimize_scalar(_find_m_minimizer, args=(si, p, r, v0_spline_out), bracket=(0, s1), method="bounded",
+                                          bounds=(0, s1))
+        # res = optimize.minimize_scalar(_find_m, args=(si, p.beta, p.theta_m, p.theta_n, p.eta, p.s_grid,
+        #                                               p.store_cost,  r.p_star, r.vfunc0, r.wage), bracket=(0, s1), method="bounded",
+        #                                   bounds=(0, s1), tol=1e-10)
 
         r.m_arr[si] = res.x
-    mx = r.m_arr > p.s_grid  # check m* < s1
-    r.m_arr[mx] = p.s_grid[mx]
-    r.n_arr = _find_n(r.m_arr, p.theta_m, p.theta_n, p.eta, r.p_star)
-    r.vals_s1_new =r.p_star*_final_net_prod(r.m_arr, r.n_arr, p.theta_m,
-                             p.theta_n, p.s_grid, p.store_cost, r.wage) + p.beta*v0_spline(p.s_grid-r.m_arr)
-    # check corner solution
-    vals_exhaust = r.p_star*_final_net_prod(p.s_grid, _find_n(p.s_grid, p.theta_m, p.theta_n, p.eta, r.p_star), p.theta_m,
-                             p.theta_n, p.s_grid, p.store_cost, r.wage) + p.beta*v0_spline(0)
-    exhaust_mx = vals_exhaust > r.vals_s1_new
-    r.vals_s1_new[exhaust_mx] = vals_exhaust[exhaust_mx]
+    r.vals_s1_new, r.n_arr = _mask_v0(r.m_arr, p.s_grid, p.theta_m, p.theta_n, p.eta, r.p_star, p.store_cost, r.wage, v0_spline_out(0), v0_spline_out(p.s_grid-r.m_arr), p.beta)
 
 @njit
-def _init_guess_expected_v0(s_grid, p_star, theta_m, theta_n, eta):
+def _mask_v0(m_arr, s_grid, theta_m, theta_n, eta, p_star, store_cost, wage, corner, next_val, beta):
+    mx = m_arr > s_grid  # check m* < s1
+    m_arr[mx] = s_grid[mx]
+    n_arr = _find_n(m_arr, theta_m, theta_n, eta, p_star)
+    vals_s1_new =p_star*_final_net_prod(m_arr, n_arr, theta_m,
+                             theta_n, s_grid, store_cost, wage) + beta*next_val
+    # check corner solution
+    vals_exhaust = p_star*_final_net_prod(s_grid, _find_n(s_grid, theta_m, theta_n, eta, p_star), theta_m,
+                             theta_n, s_grid, store_cost, wage) + beta*corner
+    exhaust_mx = vals_exhaust > vals_s1_new
+    vals_s1_new[exhaust_mx] = vals_exhaust[exhaust_mx]
+    return vals_s1_new, n_arr
+
+@njit
+def _find_m(m, si, beta, theta_m,theta_n, eta, s_grid, store_cost,  p_star, vfunc0, wage):
+    n = _find_n(m, theta_m, theta_n, eta, p_star)
+    this_val = p_star*_final_net_prod(m, n, theta_m, theta_n, s_grid[si], store_cost, wage)
+    next_u = beta*np.interp(s_grid[si]-m, s_grid, vfunc0)
+    return -1.*(this_val+next_u)  # because the function is minimized
+
+@njit
+def init_guess_expected_v0(s_grid, p_star, theta_m, theta_n, eta):
     # returns arr of length NS 
     inv_the_n = 1-theta_n
     first = p_star**(1/(1-theta_m))
